221a95b619de26fd127ed614a1289d05b2d6fc2e
Andy Teucher <andy.teucher@gov.bc.ca>
2021-11-05 12:04:42 -0700

    Add sticker

---
+++ inst/sticker/README.md
@@ -0,0 +1,4 @@
+Image obtained from: https://commons.wikimedia.org/wiki/File:Robert_Havell_after_John_James_Audubon,_American_Water_Ouzel,_1837,_NGA_32511.jpg
+
+Copyright holder: https://creativecommons.org/publicdomain/zero/1.0/
+Image title: "Robert Havell after John James Audubon, American Water Ouzel, 1837, hand-colored engraving and aquatint on Whatman wove paper, Gift of Mrs. Walter B. James, 1945.8.370"
---
+++ inst/sticker/sticker.R
@@ -0,0 +1,28 @@
+library(hexSticker)
+library(magick)
+
+filename <- "inst/sticker/JJA_AMDI.jpg"
+
+download.file('https://upload.wikimedia.org/wikipedia/commons/1/10/Robert_Havell_after_John_James_Audubon%2C_American_Water_Ouzel%2C_1837%2C_NGA_32511.jpg',
+              destfile = filename)
+
+img <- image_read(filename)
+img_data <- image_data(img)
+ratio <- dim(img_data)[2] / dim(img_data)[3]
+
+height <- 1.2
+width <- height * ratio
+
+font_path <- switch (Sys.info()['sysname'],
+                     Darwin = "/Library/Fonts/BCSans-Regular.ttf",
+                     Windows = "C:/WINDOWS/FONTS/GOTHIC.TTF"
+)
+
+sysfonts::font_add("BC Sans", font_path)
+
+sticker(filename, package="dipr",
+        s_x=1, s_y=0.75, s_height = height, s_width = width,
+        p_size=24, p_color = "#605B55", p_family = "BC Sans",
+        h_color = "#9A7F64", h_size = 1.3,
+        white_around_sticker = TRUE,
+        filename="inst/sticker/sticker.png")
