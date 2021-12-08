853070868acaba313871a9308217559cd643708d
Sam Albers <sam.albers@gov.bc.ca>
2021-12-07 16:20:36 -0800

    Update inst/templates/_targets.R
    Co-authored-by: Bonnie Robert <bonnie.robert@gov.bc.ca>

--- inst/templates/_targets.R
+++ inst/templates/_targets.R
@@ -27,7 +27,11 @@ if (.Platform$OS.type == "windows") options("arrow.use_threads" = FALSE)
 
 ## Load packages specified in DESCRIPTION file
 tar_option_set(packages = desc::desc_get_deps()$package)
+## To debug a target set the target:
+# tar_option_set(debug = "target_to_debug")
 
+## And run
+# tar_make(callr_function = NULL, names = "target_to_debug", shortcut = TRUE)
 
 ## paths
 paths <- list(
